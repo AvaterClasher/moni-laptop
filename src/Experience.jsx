26c8cf77bb2f0ec263fd3e12fe59a208a76a0728
Soumyadip Moni <116944847+AvaterClasher@users.noreply.github.com>
2023-02-24 01:42:13 +0530

    Update Experience.jsx

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import {Text, ContactShadows, PresentationControls,Float,Environment, useGLTF, Html } from "@react-three/drei";
+import {useAspect,Text, ContactShadows, PresentationControls,Float,Environment, useGLTF, Html } from "@react-three/drei";
 
 export default function Experience() {
   const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf');
